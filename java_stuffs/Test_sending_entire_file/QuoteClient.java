81b4912d167276017d541935553dddba769d73d8
Stefan Daniel Van Deventer <20273401@sun.ac.za>
2018-08-16 16:51:28 +0200

    Added everything so far, can send a file via packets

--- java_stuffs/Test_sending_entire_file/QuoteClient.java
+++ java_stuffs/Test_sending_entire_file/QuoteClient.java
@@ -5,10 +5,14 @@ import java.util.Timer;
 import java.util.TimerTask;
 
 public class QuoteClient {
-	//boolean ontime = false;
-	//TimerTask task = new TimerTask(){
+
 	public static void main(String[] args) throws IOException {
 		double numPacs;
+		String packets_received = "";
+
+		File receivedFile;
+		FileOutputStream output;// = new FileOutputStream("Output.txt");
+
 		if (args.length < 1) {
 			System.out.println("java QuoteClient <hostname>");
 			return;
@@ -19,6 +23,7 @@ public class QuoteClient {
 
 		//send request
 		byte[] buf = new byte[256];
+		byte[] info = new byte[100];
 		InetAddress address = InetAddress.getByName(args[0]);
 		DatagramPacket packet = new DatagramPacket(buf, buf.length, address, 8000);
 		socket.send(packet);
@@ -27,24 +32,41 @@ public class QuoteClient {
 		//packet = new DatagramPacket(buf, buf.length);
 		//socket.receive(packet);
 
-		//Timer timer = new Timer();
 
-		for (int i=0; i<12; i++) {
-			//timer.schedule(task, 1000);
+		String infoStr = "";
+		String receivedData = "";
+		String pacnr = "";
+		
+		/*
+		 * Receive the info of the file being transferred i.e. .txt or .mp3
+		 */
+		packet = new DatagramPacket(info, info.length);
+		socket.receive(packet);
+
+		infoStr = new String(packet.getData(), 0, packet.getLength());
+
+		receivedFile = new File(infoStr.substring(infoStr.indexOf(",")+2)+"."+infoStr.substring(0, infoStr.indexOf(",")));
+		output = new FileOutputStream(receivedFile);
+
+		/*
+		 * Receive the actual file in packets
+		 */
+		for (int i=0; i<11; i++) {
 
 			packet = new DatagramPacket(buf, buf.length);
 			socket.receive(packet);
-			//ontime = true;
-			//timer.cancel();
-			String received = new String(packet.getData(), 0, packet.getLength());
-			System.out.println("Quote nr "+i+": " + received);
+
+			pacnr = new String(packet.getData(), 0, 3);
+			packets_received = packets_received + pacnr+",";
+
+			receivedData = new String(packet.getData(), 3, packet.getLength()-3);
+			output.write(packet.getData(), 3, packet.getLength()-3);
+			//System.out.println("Received packet nr."+ pacnr);
+			System.out.print(receivedData);
 		}
-
+		packets_received = "";
+		output.close();
 		 
-		/*if (!ontime) {
-			System.out.println("It took to long!");
-		}*/
-
 		//display response
 		//String received = new String(packet.getData(), 0, packet.getLength());
 		//System.out.println("Quote of the moment: " + received);
@@ -52,6 +74,5 @@ public class QuoteClient {
 		socket.close();
 
 	}
-	//};
 
 }
--- java_stuffs/Test_sending_entire_file/QuoteServerThread.java
+++ java_stuffs/Test_sending_entire_file/QuoteServerThread.java
@@ -32,22 +32,48 @@ public class QuoteServerThread extends Thread {
 		int count;
 		//while (moreQuotes) {
 			try {
-				byte[] buf = new byte[256];
+				byte[] buf = new byte[259];
+				byte[] pac = new byte[253];
+				byte[] seq = new byte[6];
+				byte[] info = new byte[100];
+				String seqString;
 
 				//Receive request
 				DatagramPacket packet = new DatagramPacket(buf, buf.length);
 				socket.receive(packet);
 
+				InetAddress address = packet.getAddress();
+				int port = packet.getPort();
 
-				double numPacs = Math.ceil((int)(myFile.length())/256);
+				/*
+				 * Send the information
+				 */
+				info = ("txt, receivedFile").getBytes();
+				packet = new DatagramPacket(info, info.length, address, port);
+				socket.send(packet);
+
+
+				double numPacs = Math.ceil((int)(myFile.length())/259);
 				System.out.println(numPacs+"");
 
 				for (double i=0.0; i < numPacs+1; i++) {
-					bis.read(buf, 0, buf.length);
+					buf = new byte[259];
+					pac = new byte[253];
+					if (i < 10) {
+						System.out.println((int)(i)+"");
+						seqString = "00"+(int)(i);
+					} else {
+						seqString = "0"+(int)(i);
+					}
+
+					seq = seqString.getBytes();
+					bis.read(pac, 0, pac.length);
+					System.arraycopy(seq, 0, buf, 0, seq.length);
+					System.arraycopy(pac, 0, buf, seq.length, pac.length);
 					System.out.println("Packet nr."+(i+1));
 					System.out.println("Bytes sent: " + (i*buf.length));
-					InetAddress address = packet.getAddress();
-					int port = packet.getPort();
+					//InetAddress address = packet.getAddress();
+					//int port = packet.getPort();
 
 					packet = new DatagramPacket(buf, buf.length, address, port);
 					socket.send(packet);
